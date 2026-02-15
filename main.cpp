319dba6de130794acb7728b8d06178d4c58c4fe1
Krishna Rathnam <kr130205@gmail.com>
2026-02-16 01:37:00 +0530

    cleaner code

--- main.cpp
+++ main.cpp
@@ -9,49 +9,48 @@
 #include <unordered_set>
 #include <utility>
 
+std::string training_root = "./train/";
+std::string test_path = "./test/enron6/ham/";
+
 std::pair<int, int>
-calculate_score(std::unordered_map<std::string, int> &content_freq,
+calculate_score(std::unordered_map<std::string, int> &doc_freq,
                 std::unordered_map<std::string, int> &ham_freq,
-                std::unordered_map<std::string, int> &spam_freq, int spam_count,
-                int ham_count, int vocab_size, float log_ham_probability,
-                float log_spam_probability) {
+                std::unordered_map<std::string, int> &spam_freq, int spam_total,
+                int ham_total, int vocab_size, float log_ham_prior,
+                float log_spam_prior) {
+  double ham_log_sum = 0.0;
+  double spam_log_sum = 0.0;
 
-  double ham_dp = 0.0;
-  double spam_dp = 0.0;
-  for (auto [word, count] : content_freq) {
+  for (auto [word, count] : doc_freq) {
+    spam_log_sum += count * std::log((spam_freq[word] + 1.0) /
+                                     (double)(spam_total + vocab_size));
 
-    spam_dp += count * std::log((spam_freq[word] + 1.0) /
-                                (double)(spam_count + vocab_size));
-
-    ham_dp += count * std::log((ham_freq[word] + 1.0) /
-                               (double)(ham_count + vocab_size));
+    ham_log_sum += count * std::log((ham_freq[word] + 1.0) /
+                                    (double)(ham_total + vocab_size));
   }
 
-  double spam_score = log_spam_probability + spam_dp;
-  double ham_score = log_ham_probability + ham_dp;
+  double ham_score = log_ham_prior + ham_log_sum;
+  double spam_score = log_spam_prior + spam_log_sum;
 
-  auto p = std::make_pair(ham_score, spam_score);
-
-  return p;
+  return {ham_score, spam_score};
 }
 
 int main() {
-  std::string path = "./preprocessed/";
 
   std::unordered_map<std::string, int> ham_freq;
   std::unordered_map<std::string, int> spam_freq;
 
-  auto counts = walkDirectory(path, spam_freq, ham_freq);
-  int ham_count = counts.first;
-  int spam_count = counts.second;
+  auto counts = walkDirectory(training_root, spam_freq, ham_freq);
 
-  int total_count = ham_count + spam_count;
+  int ham_total = counts.first;
+  int spam_total = counts.second;
+  int total_docs = ham_total + spam_total;
 
-  float ham_probability = float(ham_count) / total_count;
-  float spam_probability = float(spam_count) / total_count;
+  float ham_prior = float(ham_total) / total_docs;
+  float spam_prior = float(spam_total) / total_docs;
 
-  float log_ham_probability = std::log(ham_probability);
-  float log_spam_probability = std::log(spam_probability);
+  float log_ham_prior = std::log(ham_prior);
+  float log_spam_prior = std::log(spam_prior);
 
   std::unordered_set<std::string> vocab;
 
@@ -62,23 +61,48 @@ int main() {
 
   int vocab_size = vocab.size();
 
-  for (auto &entry : std::filesystem::recursive_directory_iterator(
-           "./preprocessed/enron6/spam/")) {
-    if (entry.is_regular_file()) {
-      std::ifstream file(entry.path());
-      if (file) {
-        std::unordered_map<std::string, int> content_freq;
-        int content_count = get_token(file, content_freq);
-        auto score = calculate_score(content_freq, ham_freq, spam_freq,
-                                     spam_count, ham_count, vocab_size,
-                                     log_ham_probability, log_spam_probability);
+  int ham_count = 0;
+  int spam_count = 0;
+  for (auto &entry : std::filesystem::recursive_directory_iterator(test_path)) {
+    if (!entry.is_regular_file())
+      continue;
 
-        double ham_score = score.first;
-        double spam_score = score.second;
+    std::ifstream file(entry.path());
+    if (!file)
+      continue;
 
-        std::cout << entry.path().string() << " => "
-                  << ((ham_score > spam_score) ? "HAM\n" : "SPAM\n");
-      }
+    std::unordered_map<std::string, int> doc_freq;
+    get_token(file, doc_freq);
+
+    auto [ham_score, spam_score] =
+        calculate_score(doc_freq, ham_freq, spam_freq, spam_total, ham_total,
+                        vocab_size, log_ham_prior, log_spam_prior);
+
+    // std::cout << entry.path().string() << " => ";
+    if (ham_score > spam_score) {
+      //  std::cout << "HAM\n";
+      ham_count++;
+    } else {
+      // std::cout << "SPAM\n";
+      spam_count++;
     }
+  }
+
+  std::cout << "\n";
+  std::cout << "\n";
+  std::cout << "RESULT: \n";
+
+  if (ham_count > spam_count) {
+    std::cout << "probability of ham => "
+              << float(ham_count) / float(ham_count + spam_count) << std::endl;
+
+    std::cout << "probability of spam => "
+              << float(spam_count) / float(ham_count + spam_count) << std::endl;
+  } else {
+    std::cout << "probability of spam => "
+              << float(spam_count) / float(ham_count + spam_count) << std::endl;
+
+    std::cout << "probability of ham => "
+              << float(ham_count) / float(ham_count + spam_count) << std::endl;
   }
 }
--- walkDirectory.cpp
+++ walkDirectory.cpp
@@ -9,17 +9,11 @@ std::pair<int, int> walkDirectory(std::string &path,
                                   std::unordered_map<std::string, int> &ham) {
   int ham_count = 0;
   int spam_count = 0;
-  int max_files = 15000;
-  int file_seen = 0;
-  bool is_ham = false;
   for (auto &entry : std::filesystem::recursive_directory_iterator(path)) {
-    if (file_seen > max_files)
-      break;
     if (entry.is_regular_file()) {
       std::filesystem::path p(entry.path().string());
       std::ifstream file(entry.path());
       if (file) {
-        file_seen++;
         std::cout << "trained file: " << entry.path().string() << std::endl;
         if (p.parent_path().filename().string() == "ham") {
           ham_count += get_token(file, ham);
